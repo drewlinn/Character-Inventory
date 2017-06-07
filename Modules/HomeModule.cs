6df1e2f5acef059fc9e4ee8b8ec94d886fb76c22
Andrew Dalton and Charles Emrich <andrew+charles@>
2017-06-06 17:00:24 -0700

    begin building the project into a Nancy application.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,16 +1,17 @@
-// using Nancy;
-// using Inventory.Objects;
-// using System.Collections.Generic;
-//
-// namespace Inventory
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get["/"] = _ => {
-//         return View["index.cshtml"];
-//       }
-//     }
-//   }
-// }
+using Nancy;
+using Inventory.Objects;
+using System.Collections.Generic;
+
+namespace Inventory
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        Dictionary<string, object> model = new Dictionary<string, object> {{"items", Item.GetAll()}, {"categories", Category.GetAll()}};
+        return View["index.cshtml", model];
+      };
+    }
+  }
+}
